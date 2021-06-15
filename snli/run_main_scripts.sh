9f9fc74909be020cdb959c1af0c77c5216d8ceba
Swapnil Parekh <swapnilbp100@gmail.com>
2021-06-15 16:35:42 +0530

    Create run_main_scripts.sh

---
+++ snli/run_main_scripts.sh
@@ -0,0 +1,7 @@
+python3 snli_main.py -g "neutral" -t "1" -r "1" -df "1" -oh "0"
+python3 snli_main.py -g "entailment" -t "2" -r "1" -df "1" -oh "0"
+python3 snli_main.py -g "contradiction" -t "0" -r "1" -df "1" -oh "0"
+
+python3 snli_main.py -g "neutral" -t "1" -r "1" -df "1" -oh "1"
+python3 snli_main.py -g "entailment" -t "2" -r "1" -df "1" -oh "1"
+python3 snli_main.py -g "contradiction" -t "0" -r "1" -df "1" -oh "1"
